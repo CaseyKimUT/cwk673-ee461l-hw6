aec04a34dd75a9c5789c1f4e2838832559e6dd34
Casey K <caseykim5515@utexas.edu>
2022-03-31 00:25:08 -0700

    initial hw 6 commit

---
+++ server/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+#from flask_cors import CORS
+
+app = Flask(__name__, static_folder="../client/build", static_url_path="")
+
+#CORS(app)
+
+@app.route('/name/<fname>', methods=["GET"])
+def user_last_name(fname):
+    lname = "User Not Found"
+    if(fname == 'Casey'):
+        lname = "Kim"
+    return jsonify(lnameOut=lname)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
