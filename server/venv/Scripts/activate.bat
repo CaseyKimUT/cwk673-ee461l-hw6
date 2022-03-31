8e128a82569850d278708026049cf585de326d6d
Casey K <caseykim5515@utexas.edu>
2022-03-31 00:49:04 -0700

    fix requirements and dependecies

--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kiman\python\EE461L\hw6\server\venv
+set VIRTUAL_ENV=C:\Users\kiman\python\EE461L\hw6\cwk673-ee461l-hw6\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
