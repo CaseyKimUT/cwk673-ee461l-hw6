2503a39afc2600ce60c6bd8453622acb87dfca5d
Casey K <caseykim5515@utexas.edu>
2022-03-31 01:39:05 -0700

    evewn more changes

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
+from flask.helpers import send_from_directory
 #from flask_cors import CORS
 
-app = Flask(__name__, static_folder="./client/build", static_url_path="/")
+app = Flask(__name__, static_folder="./client/build", static_url_path="")
 
 #CORS(app)
 
@@ -14,7 +15,7 @@ def user_last_name(fname):
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
